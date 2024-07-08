e032fee53acc95acc7c06acb4853beb7d8fbf410
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2024-07-08 13:52:12 +0530

    Selenium UI interactions - Added explicit wait and testng lifecycle

--- src/test/java/com/selenium/ui/interactions/UIElementsInteractionTests.java
+++ src/test/java/com/selenium/ui/interactions/UIElementsInteractionTests.java
@@ -4,10 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class UIElementsInteractionTests {
 
@@ -47,6 +51,15 @@ public class UIElementsInteractionTests {
                 System.out.println(cellText);
             }
         }
+    }
+
+    @Test
+    public void explicititWaitExample() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(180));
+        driver.get("https://www.hyrtutorials.com/p/waits-demo.html");
+        driver.findElement(By.cssSelector("#btn1")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("txt1")));
+        driver.findElement(By.id("txt1")).sendKeys("abcd");
     }
 
     @AfterMethod

e7f5fbb7b74cbdd822e04bf210c793c66b8f18c7
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2024-07-18 09:42:51 +0530

    Added TestNG Listeners Examples

---
+++ src/test/java/com/selenium/ui/interactions/sanitysuite/Test2.java
@@ -0,0 +1,13 @@
+package com.selenium.ui.interactions.sanitysuite;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+    @Test
+    public void simpleTest2() {
+        System.out.println("I am another test");
+        Assert.assertTrue(true);
+    }
+}
---
+++ src/test/java/com/selenium/ui/interactions/sanitysuite/Test3.java
@@ -0,0 +1,13 @@
+package com.selenium.ui.interactions.sanitysuite;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test3 {
+
+    @Test
+    public void simpleTest3() {
+        System.out.println("I am another test");
+        Assert.assertTrue(false);
+    }
+}
